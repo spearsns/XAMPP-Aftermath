a045547a4cff9185128860563ed95e2e17210542
Nick Spears <spears.ns@gmail.com>
2020-03-12 03:11:15 -0400

    Added meta author

--- index.php
+++ index.php
@@ -35,6 +35,7 @@
     	<meta charset="utf-8">
     	<meta http-equiv="x-ua-compatible" content="ie=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="author" content="Nicholas Spears">
     	<title>Welcome to the Aftermath</title>
 	    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
     	<link rel="stylesheet" type="text/css" href="css/styles.css" />
