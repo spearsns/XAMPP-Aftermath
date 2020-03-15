6791c10aa4ce1c5aa56101e584161b4f06c00d02
Nick Spears <spears.ns@gmail.com>
2020-03-15 14:59:05 -0400

    2 service yaml again

--- inc/config.php
+++ inc/config.php
@@ -1,6 +1,6 @@
 <?php 
    
-   $conn = mysqli_connect("35.199.2.20", 'root', '@ftermathDB6947', "aftermath");
+   $conn = mysqli_connect("http://35.199.2.20", 'root', '@ftermathDB6947', "aftermath");
    if(! $conn )
    {
      die('Could not connect: ' . mysql_error());
